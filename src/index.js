24c00468b6471f4681d4535016d247930d946509
Rafael Fernando <rafaelfernandoasilva@gmail.com>
2021-11-13 10:31:28 -0300

    Script Project

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+7;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5F5F5',
+  },
+  title: {
+    fontSize: 24,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Agência digital wds</Text>
+    </View>
+  );
+}
