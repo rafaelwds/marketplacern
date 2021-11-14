1716c0b097bdd42739cfd61868c5859cecc3ed63
Rafael Fernando <rafaelfernandoasilva@gmail.com>
2021-11-13 21:01:33 -0300

    implementation of styled-component

--- src/pages/Catalog/index.js
+++ src/pages/Catalog/index.js
@@ -1,24 +1,6 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-7;
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5F5F5",
-  },
-  title: {
-    fontSize: 24,
-    textAlign: "center",
-    margin: 10,
-  },
-});
+import { Container } from "./styles";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Catalog</Text>
-    </View>
-  );
+  return <Container />;
 }
