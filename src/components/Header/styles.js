efabaf6478b3a1846b21726d5fc1e13bcdf41552
rafael <raaelfernandoasilva@gmail.com>
2021-11-15 11:13:32 -0300

    styling the catalog page in styled component

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -3,6 +3,6 @@ import styled from "styled-components/native";
 export const Container = styled.View`
   flex-direction: row;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
   padding-top: 10px;
 `;
